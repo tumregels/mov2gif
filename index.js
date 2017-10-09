5e5c0ed3cb0ccd05bab5c02dd613c5f431c79244
Tigran Ghasabyan <Ghasabyan@Tigrans-MacBook-Pro.local>
2017-10-09 10:19:46 +0400

    add formatting

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const child_process = require('child_process');
 const gifsicle = require('gifsicle');
-const ffmpeg = require('ffmpeg-binaries');
+const ffmpeg = require('ffmpeg-binaries').ffmpegPath();
 
-child_process.exec(ffmpeg.ffmpegPath() + ' -i in.mov -s 600x400 -pix_fmt rgb24 -r 10 -f gif - | ' + gifsicle + ' --optimize=3 --delay=3 > out.gif', function (error, stdout, stderr) {
-    console.log(error || stdout || stderr);
+
+child_process.exec(`${ffmpeg} -i in.mov -s 600x400 -pix_fmt rgb24 -r 10 -f gif - | ${gifsicle} --optimize=3 --delay=3 > out.gif`, function (error, stdout, stderr) {
+    if (err) {
+        console.error(`exec error: ${err}`);
+        return;
+    }
+    console.log(stdout || stderr);
 });
