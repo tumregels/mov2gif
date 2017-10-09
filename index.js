10e593cf5db4b89b8b5b168053a2524c8a76410a
Tigran Ghasabyan <Ghasabyan@Tigrans-MacBook-Pro.local>
2017-10-09 10:35:05 +0400

    fix typo and adjust gifsicle delay

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const gifsicle = require('gifsicle');
 const ffmpeg = require('ffmpeg-binaries').ffmpegPath();
 
 
-child_process.exec(`${ffmpeg} -i in.mov -s 600x400 -pix_fmt rgb24 -r 10 -f gif - | ${gifsicle} --optimize=3 --delay=3 > out.gif`, function (error, stdout, stderr) {
-    if (err) {
-        console.error(`exec error: ${err}`);
+child_process.exec(`${ffmpeg} -i in.mov -s 600x400 -pix_fmt rgb24 -r 10 -f gif - | ${gifsicle} --optimize=3 --delay=10 > out.gif`, function (error, stdout, stderr) {
+    if (error) {
+        console.error(`exec error: ${error}`);
         return;
     }
     console.log(stdout || stderr);
